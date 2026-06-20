a543284ccd6e9830f2d74e16091baf901bacf9f1
nabrajsubedi52-cell <nabrajsubedi52@gmail.com>
2026-06-20 22:31:15 +0545

    added index file

---
+++ design.js
@@ -0,0 +1,8 @@
+// import Typed from 'typed.js';
+// var typed= new typed(".modify", {
+//       String:["Web Designer","Graphics Designer"],
+//       typeSpeed:100,
+//       backSpeed:100,
+//       backDelay:1000,
+//       loop:true
+// });
